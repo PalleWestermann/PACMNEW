d0aa7c900cd83ea0b88dd0843d45d05fec69470f
Palle Westermann <pwe@tec.dk>
2026-04-22 17:36:13 +0200

    Add project files.

---
+++ PACM.BL/Customer.cs
@@ -0,0 +1,30 @@
+﻿namespace PACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string EmailAddress { get; set; }
+
+        // FirstName uses Auto-implemented property (for when you do not need to check on Set and Get).
+        public string FirstName { get; set; }
+
+        public string FullName {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
+
+        // Property LastName uses a private 'backing' field, _lastName.
+        // (for when you need to check on Set and Get, fx. Validating or Authenticating).
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }   // Get accessor returns the value of the private field _lastName.
+            set { _lastName = value; }  // Value is a keyword that represents the value being assigned to the property.
+        }
+
+
+    }
+}
