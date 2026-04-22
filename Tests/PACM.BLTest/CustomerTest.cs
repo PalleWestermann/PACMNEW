1a040038e2087edfb3be30400c1899d868276a0c
Palle Westermann <pwe@tec.dk>
2026-04-22 17:54:04 +0200

    First Test Ready, FullName Valid

---
+++ Tests/PACM.BLTest/CustomerTest.cs
@@ -0,0 +1,26 @@
+﻿using PACM.BL;
+
+namespace PACM.BLTest
+{
+    [TestClass]
+    public sealed class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // Arrange
+            //// Using an object initializer to create a Test Customer Object
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            
+            string expected = "Baggins, Bilbo"; // Variable to hold the expected value result when actually running the FullName Property
+            // Act
+            string actual = customer.FullName;  // Actually performing the operation we are testing for.
+            // Assert
+            Assert.AreEqual(expected, actual);  // Verifying the expected result is the same as the actual result.
+        }
+    }
+}
