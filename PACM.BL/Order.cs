9703c345d3d1ad3b8f3e380071f071dd121c49c3
Palle Westermann <pwe@tec.dk>
2026-04-23 06:25:47 +0200

    Just finished creating the rest of the classes.

---
+++ PACM.BL/Order.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PACM.BL
+{
+    public class Order
+    {
+        public Order()
+        {
+            
+        }
+
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; private set; }
+
+        /// <summary>
+        /// Retrieve one order.
+        /// </summary>  
+        /// 
+        public Order Retrieve (int orderId)
+        {
+            // Code that retrieves the defined order.
+            return new Order();
+        }
+
+        /// <summary>
+        /// Saves the current order.
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // Code that saves the defined order.
+            return true;
+        }
+
+        /// <summary>
+        ///  Determines whether the current object is in a valid state for processing.
+        /// </summary>
+        /// <returns>true if the object passes validation checks; otherwise, false.</returns>
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null) isValid = false;
+            return isValid;
+        }
+    }
+}
---
+++ PACM.BL/OrderItem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PACM.BL
+{
+    public class OrderItem
+    {
+        public OrderItem()
+        {
+            
+        }
+
+        public OrderItem(int orderItemId)
+        {
+            OrderItemId = orderItemId;
+        }
+
+        public int OrderItemId { get; private set; }
+        public int ProductId { get; private set; }
+        public decimal? PurchasePrice { get; set; }
+        public int Quantity { get; set; }
+
+        /// <summary>
+        ///     Retrieves the order item with the specified identifier.
+        /// </summary>
+        /// <param name="orderItemId">The unique identifier of the order item to retrieve.</param>
+        /// <returns>An OrderItem representing the requested order item.</returns>
+        public OrderItem Retrieve(int orderItemId)
+        {
+            // Code that retrieves the defined order item.
+            return new OrderItem();
+        }
+
+        /// <summary>
+        ///     Saves the current order item to the underlying data store.
+        /// </summary>
+        /// <returns>true if the order item was saved successfully; otherwise, false.</returns>
+        public bool Save()
+        {
+            // Code that saves the defined order item.
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if (ProductId <= 0) isValid = false;
+            if (PurchasePrice == null) isValid = false;
+            if (Quantity <= 0) isValid = false;
+            return isValid;
+        }
+    }
+}
