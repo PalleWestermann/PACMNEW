bfc241b01138ad0a09d035dffe860c0734fd921f
Palle Westermann <pwe@tec.dk>
2026-04-26 10:59:52 +0200

    Created and tested Stringhandler Method alone with new Test Project.

---
+++ PACM.Common/StringHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace PACM.Common
+{
+    public class StringHandler
+    {
+        public string InsertSpaces(string source)
+        {
+            var result = string.Empty;
+            if (!string.IsNullOrWhiteSpace(source))
+            {
+                foreach (var letter in source)
+                {
+                    if (char.IsUpper(letter))
+                    {
+                        result = result.Trim(); // Trim any spaces already there.
+                        result += " ";
+                    }
+                    result += letter;
+                }
+            }
+            result = result.Trim();
+            return result;
+        }
+    }
+}
